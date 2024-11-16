386c559a3d7c763e5773cc20bdf2e3f39a96c9f5
Grenghis-Khan <63885013+Grenghis-Khan@users.noreply.github.com>
2024-11-16 07:44:13 -0700

    revert back to original contextMenu options

--- content.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("keydown", function (event) {
-  // Send key state to background script
-  chrome.runtime.sendMessage({
-    type: "keyEvent",
-    metaKey: event.metaKey,
-    shiftKey: event.shiftKey,
-  });
-});
